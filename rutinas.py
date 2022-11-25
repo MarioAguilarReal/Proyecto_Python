3635a0353ca6abea0eecd84d686b284e6093635e
MarioAguilarReal <94491813+MarioAguilarReal@users.noreply.github.com>
2022-11-24 23:18:34 -0800

    Add news rutines

--- rutinas.py
+++ rutinas.py
@@ -1,8 +1,82 @@
+import time
+from Servo import *
+servo=Servo()
+
 def rutina1():
     print("Hola soy la rutina 1")
+    try:
+        for i in range(90):
+            servo.setServoAngle(4,90-i)
+            servo.setServoAngle(7,90-i)
+            servo.setServoAngle(8,90+i)
+            servo.setServoAngle(11,90+i)
+            time.sleep(0.01)
+
+        for i in range(90):
+            servo.setServoAngle(2,90-i)
+            servo.setServoAngle(5,90-i)
+            servo.setServoAngle(10,90+i)
+            servo.setServoAngle(13,90+i)
+            time.sleep(0.01)
+        for i in range(60):
+            servo.setServoAngle(3,90-i)
+            servo.setServoAngle(6,90-i)
+            servo.setServoAngle(9,90+i)
+            servo.setServoAngle(12,90+i)
+            time.sleep(0.01)
+        print ("\nEnd of program")
+    except KeyboardInterrupt:
+        print ("\nEnd of program")
 
 def rutina2():
     print("Hola soy la rutina 2")
+    try:
+        for i in range(90):
+            servo.setServoAngle(4,90-i)
+            servo.setServoAngle(7,90-i)
+            servo.setServoAngle(8,90+i)
+            servo.setServoAngle(11,90+i)
+            time.sleep(0.01)
+
+        for i in range(90):
+            servo.setServoAngle(2,90-i)
+            servo.setServoAngle(5,90-i)
+            servo.setServoAngle(10,90+i)
+            servo.setServoAngle(13,90+i)
+            time.sleep(0.01)
+        for i in range(60):
+            servo.setServoAngle(3,90-i)
+            servo.setServoAngle(6,90-i)
+            servo.setServoAngle(9,90+i)
+            servo.setServoAngle(12,90+i)
+            time.sleep(0.01)
+        print ("\nEnd of program")
+    except KeyboardInterrupt:
+        print ("\nEnd of program")
 
 def rutina3():
     print("Hola soy la rutina 3")
+    try:
+        for i in range(90):
+            servo.setServoAngle(4,90-i)
+            servo.setServoAngle(7,90-i)
+            servo.setServoAngle(8,90+i)
+            servo.setServoAngle(11,90+i)
+            time.sleep(0.01)
+
+        for i in range(90):
+            servo.setServoAngle(2,90-i)
+            servo.setServoAngle(5,90-i)
+            servo.setServoAngle(10,90+i)
+            servo.setServoAngle(13,90+i)
+            time.sleep(0.01)
+        for i in range(60):
+            servo.setServoAngle(3,90-i)
+            servo.setServoAngle(6,90-i)
+            servo.setServoAngle(9,90+i)
+            servo.setServoAngle(12,90+i)
+            time.sleep(0.01)
+        print ("\nEnd of program")
+    except KeyboardInterrupt:
+        print ("\nEnd of program")
+
