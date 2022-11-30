1120d24dfff41489b2161074854b1225faa535c5
FerUgalde <118574317+FerUgalde@users.noreply.github.com>
2022-11-30 14:04:44 -0800

    funcion regresar
    hombros

--- rutinas.py
+++ rutinas.py
@@ -142,11 +142,17 @@ def rutina3():
 
 def regresar():
     servo.setServoAngle(5,90)
-    servo.setServoAngle(6,90)
     servo.setServoAngle(13,90)
-    servo.setServoAngle(12,90)
     servo.setServoAngle(10,90)
-    servo.setServoAngle(9,90)
     servo.setServoAngle(2,90)
+    servo.setServoAngle(6,90)
+    servo.setServoAngle(12,90)
+    servo.setServoAngle(9,90)
     servo.setServoAngle(3,90)
+    servo.setServoAngle(7,90)
+    servo.setServoAngle(11,90)
+    servo.setServoAngle(8,90)
+    servo.setServoAngle(4,90)
+    
+    
  
