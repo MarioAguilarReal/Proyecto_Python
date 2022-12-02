109a9076d934c9ccfe4884f8e0d3a164663be095
Gaelongo <113254167+Gaelongo@users.noreply.github.com>
2022-12-02 11:35:43 -0800

    programa de prueba

---
+++ recto.py
@@ -0,0 +1,17 @@
+from Servo import *
+
+servo = Servo()
+
+servo.setServoAngle(13 ,90)
+servo.setServoAngle(12 ,90)
+servo.setServoAngle(11 ,90)
+servo.setServoAngle(4 ,90)
+servo.setServoAngle(3 ,90)
+servo.setServoAngle(2 ,90)
+servo.setServoAngle(10 ,90)
+servo.setServoAngle(9 ,90)
+servo.setServoAngle(8 ,80)
+servo.setServoAngle(7 ,95)
+servo.setServoAngle(6 ,90)
+servo.setServoAngle(5 ,90)
+print("End of program.")
